8f2a34caa3c90997a8c12dcbf7df51b6e2bc7552
Bondi Somboonaree <0871316@schoolsnet.act.edu.au>
2026-05-13 11:32:28 +1000

    Upd

---
+++ ITAssignment/UC_Inventory.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ITAssignment
+{
+    partial class UC_Inventory
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.dgvInventory = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Brand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gbProductDetails = new System.Windows.Forms.GroupBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.lblBrand = new System.Windows.Forms.Label();
+            this.btnClearProduct = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.gbDelete = new System.Windows.Forms.GroupBox();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            this.gbProductDetails.SuspendLayout();
+            this.gbDelete.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvInventory
+            // 
+            this.dgvInventory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Brand,
+            this.ProductName,
+            this.Price,
+            this.Quantity});
+            this.dgvInventory.Location = new System.Drawing.Point(387, 44);
+            this.dgvInventory.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvInventory.Name = "dgvInventory";
+            this.dgvInventory.ReadOnly = true;
+            this.dgvInventory.RowHeadersWidth = 51;
+            this.dgvInventory.Size = new System.Drawing.Size(567, 513);
+            this.dgvInventory.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ProductID";
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Brand
+            // 
+            this.Brand.DataPropertyName = "ProductBrand";
+            this.Brand.HeaderText = "Brand";
+            this.Brand.MinimumWidth = 6;
+            this.Brand.Name = "Brand";
+            this.Brand.ReadOnly = true;
+            // 
+            // ProductName
+            // 
+            this.ProductName.DataPropertyName = "ProductName";
+            this.ProductName.HeaderText = "Product Name";
+            this.ProductName.MinimumWidth = 6;
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.DataPropertyName = "ProductPrice";
+            this.Price.HeaderText = "Price";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "ProductQuantity";
+            this.Quantity.HeaderText = "Stock Quantity";
+            this.Quantity.MinimumWidth = 6;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.ReadOnly = true;
+            // 
+            // gbProductDetails
+            // 
+            this.gbProductDetails.Controls.Add(this.txtBrand);
+            this.gbProductDetails.Controls.Add(this.lblBrand);
+            this.gbProductDetails.Controls.Add(this.btnClearProduct);
+            this.gbProductDetails.Controls.Add(this.txtQuantity);
+            this.gbProductDetails.Controls.Add(this.lblQuantity);
+            this.gbProductDetails.Controls.Add(this.txtPrice);
+            this.gbProductDetails.Controls.Add(this.lblPrice);
+            this.gbProductDetails.Controls.Add(this.txtName);
+            this.gbProductDetails.Controls.Add(this.lblName);
+            this.gbProductDetails.Controls.Add(this.txtID);
+            this.gbProductDetails.Controls.Add(this.lblID);
+            this.gbProductDetails.Location = new System.Drawing.Point(55, 44);
+            this.gbProductDetails.Margin = new System.Windows.Forms.Padding(4);
+            this.gbProductDetails.Name = "gbProductDetails";
+            this.gbProductDetails.Padding = new System.Windows.Forms.Padding(4);
+            this.gbProductDetails.Size = new System.Drawing.Size(245, 219);
+            this.gbProductDetails.TabIndex = 1;
+            this.gbProductDetails.TabStop = false;
+            this.gbProductDetails.Text = "Product Details";
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(93, 119);
+            this.txtBrand.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(132, 20);
+            this.txtBrand.TabIndex = 9;
+            // 
+            // lblBrand
+            // 
+            this.lblBrand.AutoSize = true;
+            this.lblBrand.Location = new System.Drawing.Point(8, 123);
+            this.lblBrand.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBrand.Name = "lblBrand";
+            this.lblBrand.Size = new System.Drawing.Size(35, 13);
+            this.lblBrand.TabIndex = 8;
+            this.lblBrand.Text = "Brand";
+            // 
+            // btnClearProduct
+            // 
+            this.btnClearProduct.Location = new System.Drawing.Point(8, 183);
+            this.btnClearProduct.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClearProduct.Name = "btnClearProduct";
+            this.btnClearProduct.Size = new System.Drawing.Size(215, 28);
+            this.btnClearProduct.TabIndex = 5;
+            this.btnClearProduct.Text = "Clear";
+            this.btnClearProduct.UseVisualStyleBackColor = true;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(93, 151);
+            this.txtQuantity.Margin = new System.Windows.Forms.Padding(4);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(132, 20);
+            this.txtQuantity.TabIndex = 7;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(8, 155);
+            this.lblQuantity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 6;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(93, 87);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(132, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(8, 91);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Price";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(93, 55);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(8, 59);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(93, 23);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(132, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(8, 27);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(55, 271);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(196, 271);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(0, 23);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // gbDelete
+            // 
+            this.gbDelete.Controls.Add(this.txtDelete);
+            this.gbDelete.Controls.Add(this.btnDelete);
+            this.gbDelete.Location = new System.Drawing.Point(55, 318);
+            this.gbDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.gbDelete.Name = "gbDelete";
+            this.gbDelete.Padding = new System.Windows.Forms.Padding(4);
+            this.gbDelete.Size = new System.Drawing.Size(267, 65);
+            this.gbDelete.TabIndex = 5;
+            this.gbDelete.TabStop = false;
+            this.gbDelete.Text = "Delete Product with ID/Name";
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(108, 27);
+            this.txtDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(132, 20);
+            this.txtDelete.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(55, 510);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(159, 28);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save Changes";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // UC_Inventory
+            // 
+            this.Name = "UC_Inventory";
+            this.Load += new System.EventHandler(this.UC_Inventory_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            this.gbProductDetails.ResumeLayout(false);
+            this.gbProductDetails.PerformLayout();
+            this.gbDelete.ResumeLayout(false);
+            this.gbDelete.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInventory;
+        private System.Windows.Forms.GroupBox gbProductDetails;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button btnClearProduct;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox gbDelete;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Label lblBrand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Brand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ ITAssignment/UC_Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITAssignment
+{
+    public partial class UC_Inventory : UserControl
+    {
+        private BindingList<Product> _inventoryList = new BindingList<Product>();
+        private BindingSource _bindingSource = new BindingSource();
+        string filepath = "./product.csv";
+        public UC_Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void UC_Inventory_Load(object sender, EventArgs e)
+        {
+            var tempData = InventoryService.LoadFromCSV(filePath);
+            _inventoryList.Clear();
+
+            foreach (var item in tempData)
+            {
+                _inventoryList.Add(item);
+            }
+            dgvInventory.DataSource = _inventoryList;
+        }
+
+        private void UC_Inventory_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
